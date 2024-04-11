634d3f1e24d51bba5840d9a47cc24f6c3769c5be
Rouven Spaar <rouven.spaar@gmail.com>
2024-04-11 19:49:36 +0200

    update: adjusted metadata and chat history

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,50 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <div className={styles.chatHistory}>
-        <div className={styles.historyHeader}>
-          <div className={styles.headerTitle}>HIPPO</div>
-          <div className={styles.headerSubTitle}>Version 1.0</div>
+        <div>
+          <div className={styles.historyHeader}>
+            <div className={styles.headerTitle}>HIPPO</div>
+            <div className={styles.headerSubTitle}>Version 1.0</div>
+          </div>
+          <div className={styles.headerDivider}></div>
+          <ChatHistory></ChatHistory>
         </div>
-        <div className={styles.headerDivider}></div>
-        <ChatHistory></ChatHistory>
+        <div>
+          <div className={styles.profilePicture}></div>
+          <div className={styles.profileDivider}></div>
+          <div className={styles.profileBottom}>
+            <div>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+                <path d="M0 0h48v48h-48z" fill="none" />
+                <path d="M38.86 25.95c.08-.64.14-1.29.14-1.95s-.06-1.31-.14-1.95l4.23-3.31c.38-.3.49-.84.24-1.28l-4-6.93c-.25-.43-.77-.61-1.22-.43l-4.98 2.01c-1.03-.79-2.16-1.46-3.38-1.97l-.75-5.3c-.09-.47-.5-.84-1-.84h-8c-.5 0-.91.37-.99.84l-.75 5.3c-1.22.51-2.35 1.17-3.38 1.97l-4.98-2.01c-.45-.17-.97 0-1.22.43l-4 6.93c-.25.43-.14.97.24 1.28l4.22 3.31c-.08.64-.14 1.29-.14 1.95s.06 1.31.14 1.95l-4.22 3.31c-.38.3-.49.84-.24 1.28l4 6.93c.25.43.77.61 1.22.43l4.98-2.01c1.03.79 2.16 1.46 3.38 1.97l.75 5.3c.08.47.49.84.99.84h8c.5 0 .91-.37.99-.84l.75-5.3c1.22-.51 2.35-1.17 3.38-1.97l4.98 2.01c.45.17.97 0 1.22-.43l4-6.93c.25-.43.14-.97-.24-1.28l-4.22-3.31zm-14.86 5.05c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z" />
+              </svg>
+
+            </div>
+            <div>
+              <svg fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 471.2 471.2">
+                <g>
+                  <g>
+                    <path d="M227.619,444.2h-122.9c-33.4,0-60.5-27.2-60.5-60.5V87.5c0-33.4,27.2-60.5,60.5-60.5h124.9c7.5,0,13.5-6,13.5-13.5
+			s-6-13.5-13.5-13.5h-124.9c-48.3,0-87.5,39.3-87.5,87.5v296.2c0,48.3,39.3,87.5,87.5,87.5h122.9c7.5,0,13.5-6,13.5-13.5
+			S235.019,444.2,227.619,444.2z"/>
+                    <path d="M450.019,226.1l-85.8-85.8c-5.3-5.3-13.8-5.3-19.1,0c-5.3,5.3-5.3,13.8,0,19.1l62.8,62.8h-273.9c-7.5,0-13.5,6-13.5,13.5
+			s6,13.5,13.5,13.5h273.9l-62.8,62.8c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4l85.8-85.8
+			C455.319,239.9,455.319,231.3,450.019,226.1z"/>
+                  </g>
+                </g>
+              </svg>
+            </div>
+          </div>
+        </div>
       </div>
       <div className={styles.content}>
         <div className={styles.chat}>
           <Chat></Chat>
-          <TextInput></TextInput>
+          <div>
+            <TextInput></TextInput>
+            <div className={styles.inputHint}>For a final verdict, always contact a professional radiologist.</div>
+          </div>
         </div>
         <div className={styles.diagnosis}>
           <Scan></Scan>
