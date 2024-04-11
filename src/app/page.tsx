f05b70c39ed634fba02cc66234251306196b9420
Rouven Spaar <rouven.spaar@gmail.com>
2024-04-11 15:39:42 +0200

    update: adjusted layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,11 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <div className={styles.chatHistory}></div>
-      <div className={styles.chat}></div>
-      <div className={styles.diagnosis}>
-        <Scan></Scan>
+      <div className={styles.content}>
+        <div className={styles.chat}></div>
+        <div className={styles.diagnosis}>
+          <Scan></Scan>
+        </div>
       </div>
     </div>
   );
