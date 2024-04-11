89ea27985f940da47498e84b0235266162cc6e31
Patrick_Remerscheid <patrick.remerscheid@gmail.com>
2024-04-11 14:38:02 +0200

    feat(scan):  add scan component with template image and buttons

---
+++ src/app/components/scan.tsx
@@ -0,0 +1,21 @@
+import styles from './scan.module.css'
+
+function Scan() {
+    return(
+        <div>
+             <div>
+                <img className={styles.scan}
+                    src='/test_xray.png'
+                    alt='test scan' 
+                    />
+            </div>
+            <div className={styles.scanParameters}>
+                <div>Invert Colors</div>
+                <div>Raw Gradients</div>
+                <div>Reset Layers</div>
+            </div>
+        </div>
+    )
+}
+
+export default Scan;
