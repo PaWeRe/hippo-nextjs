3d395cd8bbfce352e3739ba3c73528bdd8f1008b
Rouven Spaar <rouven.spaar@gmail.com>
2024-04-12 09:04:06 +0200

    feat: added import and export buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,11 +88,18 @@ export default function Home() {
           <Chat scan={exampleScans[selectedIndex]}></Chat>
           <div>
             <TextInput></TextInput>
-            <div className={styles.inputHint}>For a final verdict, always contact a professional radiologist.</div>
           </div>
         </div>
         <div className={styles.diagnosis}>
           <ScanImage></ScanImage>
+          <div className={styles.importExport}>
+            <div>
+              <svg fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M21,14a1,1,0,0,0-1,1v4a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V15a1,1,0,0,0-2,0v4a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V15A1,1,0,0,0,21,14Zm-9.71,1.71a1,1,0,0,0,.33.21.94.94,0,0,0,.76,0,1,1,0,0,0,.33-.21l4-4a1,1,0,0,0-1.42-1.42L13,12.59V3a1,1,0,0,0-2,0v9.59l-2.29-2.3a1,1,0,1,0-1.42,1.42Z"/></svg>
+            </div>
+            <div>
+              <svg fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M8.71,7.71,11,5.41V15a1,1,0,0,0,2,0V5.41l2.29,2.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42l-4-4a1,1,0,0,0-.33-.21,1,1,0,0,0-.76,0,1,1,0,0,0-.33.21l-4,4A1,1,0,1,0,8.71,7.71ZM21,14a1,1,0,0,0-1,1v4a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V15a1,1,0,0,0-2,0v4a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V15A1,1,0,0,0,21,14Z" /></svg>
+            </div>
+          </div>
         </div>
       </div>
     </div>
